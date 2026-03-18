aa515a2c5577e280475762cf76b273cde9022737
Azzeddine Charef <azzeddine.charef@univ-biskra.dz>
2026-03-18 11:40:32 +0000

    wipe project

--- worker.ts
+++
@@ -1,7 +0,0 @@
-import "dotenv/config";
-import { startWorkerLoop } from "./lib/worker";
-
-startWorkerLoop().catch((error) => {
-  console.error("Worker failed to start.", error);
-  process.exit(1);
-});
